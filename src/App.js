77f5a649ae7cad36aa8f50b1edb978c1ff1735fd
bektilekdev <sbektilek@gmail.com>
2023-09-06 13:42:23 +0600

    sdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
 import Header from "./components/Header/header";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Barbers from "../src/components/Barbers/barbers";
 import Salon from "./components/Salon/salon";
 import Blog from "./components/Blog/blog";
 import Home from "./components/Home/home";
 import Footer from "./components/Footer/footer";
-
+import Salons from "./components/Home/Salons";
 
 const App = () => {
-    return (
-        <div>
-          <Header/>
-            <Routes>
-               <Route path="barbers" element={<Barbers/>}/>
-               <Route path="salon" element={<Salon/>}/>
-               <Route path="blog" element={<Blog/>}/>
-               <Route path="home" element={<Home/>}/>
-            </Routes>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      
+      <Salons />
+      <Routes>
+        <Route path="barbers" element={<Barbers />} />
+        <Route path="salon" element={<Salon />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+      {/* <Footer/> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div id='home'>
-            <div className="container">
-                <div className="home">
-                    <h1 style={{color:'white'}}>home</h1>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div id="home">
+      <div className="container">
+        <div className="home"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
