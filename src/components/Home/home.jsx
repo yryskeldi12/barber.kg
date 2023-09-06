b0bf011a165bec66125453259220c10db2cab564
sultanmamytbekov <132814069+sultanmamytbekov@users.noreply.github.com>
2023-09-06 11:36:07 +0600

    hero (#2)

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Hero from './Hero/hero';
 
 const Home = () => {
     return (
-        <div id='home'>
+        <div  id='home'>
             <div className="container">
                 <div className="home">
-                    <h1 style={{color:'white'}}>home</h1>
+                    {/* <h1 style={{color:'white'}}>home</h1> */}
+                   <Hero/>
                 </div>
             </div>
         </div>
