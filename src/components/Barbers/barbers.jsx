540f45dfbfd5abcb0264eb44e6c405780c5a64f8
Bektilek <126472077+bektilekdev@users.noreply.github.com>
2023-09-08 13:18:39 +0600

    dkj (#12)

--- src/components/Barbers/barbers.jsx
+++ src/components/Barbers/barbers.jsx
@@ -1,3 +1,23 @@
+import React from "react";
+import "./barbers.scss";
+import Master from "./Master/master";
+import Recom from "./Recom/Recom";
+
+const Barbers = () => {
+  return (
+    <div id="barbers">
+      <div className="container">
+        <div className="barbers">
+          <Master />
+          <h1 style={{ color: "white" }}>barbers</h1>
+          <div className="barbers">
+            {/* <h1 style={{color:'white'}}>barbers</h1> */}
+            <Recom />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 import React from 'react';
 import './barbers.scss'
 import Master from './Master/master';
