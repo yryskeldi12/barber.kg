c36e6384cdfad4bb10634185b361fe827d3b7c51
bektilekdev <sbektilek@gmail.com>
2023-09-08 13:17:00 +0600

    dkj

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import Hero from './Hero/hero';
-import Functional from "./functional/functional";
-import Advantages from "./advantages/advantages";
-import Platform from "./platform/platform";
 import React from "react";
-import Functional from "./functional/functional";
-import Questions from "./Questions/questions";
 import Hero from "./Hero/hero";
-import Salons from "./Salons";
+import Functional from "./functional/functional";
 import Advantages from "./advantages/advantages";
 import Platform from "./platform/platform";
-
+import Questions from "./Questions/questions";
+import Salons from "./Salons";
 
 const Home = () => {
-    return (
-        <div id='home'>
-            <div className="container">
-                <Hero/>
-                <Functional/>
-                <Advantages/>
-                <Platform/>
+  return (
+    <div id="home">
+      <div className="container">
+        <Hero />
+        <Functional />
+        <Advantages />
+        <Platform />
 
-                <Salons/>
-                <Questions/>
-            </div>
-        </div>
-    );
+        <Salons />
+        <Questions />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
