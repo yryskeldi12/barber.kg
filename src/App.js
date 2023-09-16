716d13a3a1f59f9b0a0f28f6b54267f221081c46
atantaevna <113280392+atantaevna@users.noreply.github.com>
2023-09-16 16:57:00 +0300

    newA (#22)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./components/Header/header";
 import {Route, Routes} from "react-router-dom";
 import Barbers from "../src/components/Barbers/barbers";
@@ -10,22 +10,45 @@ import Profil from "./components/Barbers/Master/Profil/profil";
 import LoginForm from "./components/loginForm/loginForm";
 import Herosalon from "./components/Salon/herosalon/herosalon";
 import Leapardsalon from "./components/Salon/leapardsalon/leapardsalon";
+import Anketa from "./components/Anketa/anketa";
+import SalonRecom from "./components/Salon/SalonRecom/salonRecom";
+import { gapi } from 'gapi-script'
+import SalonDor from "./components/Salon/SalonDor/salonDor";
+import Map from "./components/Barbers/Master/Map/map";
+import Recall from "./components/Barbers/Master/Recall/recall";
+
+const clientId = "187515703227-ipki1jn6tmq5c6dprpq5la00sqndm0aj.apps.googleusercontent.com"
 
 
  
 
 const App = () => {
+    useEffect(() => {
+        function start () {
+            gapi.client.init({
+                clientId:clientId,
+                scope:''
+            })
+        }
+        gapi.load('client:auth2',start);
+    },[])
+
+
+
     return (
         <div>
             <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="barbers" element={<Barbers/>}/>
+                <Route path="salon" element={<SalonRecom/>}/>
                 <Route path="salon" element={<Salon/>}/>
                 <Route path="blog" element={<Blog/>}/>
                 <Route path="profil" element={<Profil/>}/>
                 <Route path="enter" element={<LoginForm/>}/>
             </Routes>
+            {/*<SalonDor/>*/}
+            <Anketa/>
             <Footer/>
         </div>
     );
--- src/components/Barbers/leapard/leapard.jsx
+++ src/components/Barbers/leapard/leapard.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './leapard.scss'
 import {AiOutlineStar} from 'react-icons/ai'
-import img1 from "../leapard/img/Rectangle 156.png"
+import img from './img/men2.png'
+import img2 from './img/man1.png'
+import img3 from './img/mens2.png'
+import img1 from './img/mens1.png'
+import img4 from './img/men3.png'
+import img5 from './img/mens3.png'
+import img0 from './img/men.png'
 const Leapard = () => {
+    const [lol , setLol] = useState(false)
+    const [lol1 , setLol1] = useState(false)
+    const [lol2 , setLol2] = useState(false)
+    console.log(lol)
     return (
-        <div id="leapard">
+        <div style={{background:`url(${img0})` , backgroundColor:"#fff"}} id="leapard">
             <div  className="container">
                 <div  className="leapard">
                     <h1>барбершопы</h1>
                     <div className="leapard--text">
                         <div className='leapard--text__texts'>
-                            <div className="leapard--text__texts--block">
+                            <div onMouseOver={() => setLol(true)} onMouseLeave={() => setLol(false)} style={{background:`url(${lol ? img1 : img})` , transition:'0.5s'}} className="leapard--text__texts--block">
                                 <h3>Барбершоп</h3>
                                 <h4><AiOutlineStar style={{fontSize:"23px"}}/>4.7</h4>
                                 <div className="leapard--text__texts--block__block-text">
@@ -19,8 +29,8 @@ const Leapard = () => {
                                 </div>
 
                             </div>
-                            <div  className="leapard--text__texts--block">
-                                <img src={img1} alt=""/>
+                            <div onMouseOver={() => setLol1(true)} onMouseLeave={() => setLol1(false)}
+                                 style={{background:`url(${lol1 ? img3 : img2})`, transition:'0.5s'}}  className="leapard--text__texts--block1">
                                 <h3>Барбершоп</h3>
                                 <h4><AiOutlineStar style={{fontSize:"23px"}}/>4.5</h4>
                                 <div className="leapard--text__texts--block__block-text">
@@ -31,7 +41,7 @@ const Leapard = () => {
                             </div>
 
                         </div>
-                        <div className="leapard--text__block1">
+                        <div style={{background:`url(${lol2 ? img5 : img4})`, transition:'0.5s'}} onMouseOver={() => setLol2(true)} onMouseLeave={() => setLol2(false)} className="leapard--text__block1">
                             <h3>Барбершоп</h3>
                             <h4><AiOutlineStar style={{fontSize:"23px"}}/>4.5</h4>
                             <div className="leapard--text__block1--block-text1">
