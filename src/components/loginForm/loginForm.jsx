867c219c8030912e428ce6b7b5d3884b3400ee81
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-09-23 11:48:24 +0600

    Create a sign up form

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,117 +1,60 @@
-import React, { useState } from 'react';
-import './loginForm.scss';
-import {GoogleLogin} from "@react-oauth/google";
-import c1img from '../../../src/components/loginForm/img/c1img.png';
-import c2img from '../../../src/components/loginForm/img/c2img.png';
-import {IoIosArrowBack} from "react-icons/io";
+import React, { useState } from "react";
+import "./loginForm.scss";
+import barberImgPath from "./img/c1img.png";
+import userImgPath from "./img/c2img.png";
+import { IoIosArrowBack } from "react-icons/io";
 
-
-const clientId = "187515703227-ipki1jn6tmq5c6dprpq5la00sqndm0aj.apps.googleusercontent.com"
+const clientId =
+    "187515703227-ipki1jn6tmq5c6dprpq5la00sqndm0aj.apps.googleusercontent.com";
 
 const LoginForm = () => {
-    const clientId = "187515703227-ipki1jn6tmq5c6dprpq5la00sqndm0aj.apps.googleusercontent.com"
-    const [reverse , setReverse] = useState(true)
-    const [none , setNone] = useState(false)
-    const [isMasterMode, setIsMasterMode] = useState(true);
-    const [toggleState, setToggleState] = useState(1);
-    const oneSucces = (res) => {
-
-        console.log('Current user:', res.profileobg)
-    }
-    const onFailure = (res) => {
-        console.log('login failed',res)
-    }
-    const toggleMode = () => {
-        setIsMasterMode(!isMasterMode);
-        setToggleState(toggleState === 1 ? 2 : 1);
-    };
-    console.log(reverse)
-    let str = c1img
-    if (reverse === true){
-        str = c1img
-    }else {
-        str = c2img
-    }
-
-    const hello = toggleState === 1 ? 65 : -1370;
-    const lol = toggleState === 1 ? 0 : 729;
     return (
-        <div id="login">
-            <div className="container">
-                <div className="login">
-                    <IoIosArrowBack className="login--icon"/>
-                    <div className="login--n1">
-                        <div
-                            style={{display:reverse ? 'block' : 'none'}}
-                            className={`login--n1__a1 ${toggleState === 1}`}
-                        >
-                            <p>Вы мастер и хотите зарегистрироваться?</p>
-                            <h2 style={{color:none ? 'red' : '' ,transition:'0.8s' }} onClick={() => {
-                                toggleMode()
-                                setTimeout(() => {
-                                    setReverse(false)
-                                }, 200)
-                            }}>Для Мастеров</h2>
-                        </div>
-                        <div
-                            style={{display:reverse ? 'none' : 'block'}}
-                            className={`login--n1__a2 ${toggleState === 2}`}
-                        >
-                            <h2 style={{color:none ? 'red' : '' ,transition:'0.8s' }} onClick={() => {
-                                toggleMode()
-                                setTimeout(() => {
-                                    setReverse(true)
-                                }, 200)
-                            }}>Для Пользователя</h2>
-                        </div>
+        <div className="container">
+            <div className="user-form">
+                <div className="user-form__left-side">
+                    <div className="user-form__barber">
+                        <p className="user-form__barber-question">
+                            Вы мастер и хотите зарегистрироваться?
+                        </p>
+                        <span className="user-form__barber-title">Для Мастеров</span>
                     </div>
-                    <div className="login--cnt">
-                        <div style={{marginLeft:`${lol}px`, transition: '0.6s' , background:`url(${str})`}} className="login--cnt__images">
+                    <img className="user-form__bg-img" src={barberImgPath} alt="Master" />
+                </div>
+
+                <div className="user-form__right-side">
+                    <div className="register-form">
+                        <div className="register-form__wrap">
+                            <h2 className="register-form__title">Регистрация</h2>
+                            <span className="register-form__subtitle">Заполните данные для регистрации</span>
+                            <button className="user-form__btn user-form__btn--active register-form__enter">Вход через Google</button>
+                            <span className="register-form__extra">Или</span>
                         </div>
-                        <div style={{marginLeft:`${hello}px`, transition: '0.6s'}} className="login--cnt__c1">
-                            <h1 style={{transition:"1s"}}>{none ? 'Войти' : 'Регистрация'}</h1>
-                            <p>{none ? 'Заполните данные для того что-бы Войти' : 'Заполните данные для регистрации'}</p>
-                            <button
-                                className="login--cnt__c1--google"
-                                clientId={clientId}
-                                onSucces={oneSucces}
-                                onFailure={onFailure}
-                                cookiePolicy={'single_host'}
-                                isSignedIn={true}
-                            >Вход через Google</button>
-                            <h2>Или</h2>
+
+                        <div className="register-form__fields">
                             <input
+                                className="register-form__field"
                                 type="text"
-                                placeholder="Никнэйм*"
-                                pattern="[^,?]*"
-                                title="Пожалуйста, используйте только буквы, цифры и символы, кроме запятых и вопросительных знаков."
-                                onInput={(e) => {
-                                    const inputValue = e.target.value;
-                                    const sanitizedValue = inputValue.replace(/[?.,/'";:}{=+!%^$~_-]/g, ''); // Удаляем вопросительные знаки
-                                    e.target.value = sanitizedValue;
-                                }}
-                            />
-                            <input style={{display:none ? 'none' : 'block'}}
-                                placeholder="Адрес или Почта*"
+                                placeholder="Никнэйм"
                             />
                             <input
+                                className="register-form__field"
+                                type="text"
+                                placeholder="Адрес или Почта"
+                            />
+                            <input
+                                className="register-form__field"
                                 type="password"
                                 placeholder="Пароль"
                             />
-                            <input style={{display:none ? 'none' : 'block'}}
+                            <input
+                                className="register-form__field"
                                 type="password"
-                                placeholder="Повторить пароль*"
+                                placeholder="Повторить пароль"
                             />
-                            <div style={{marginTop : none ? '242px' : ''}} className="login--cnt__c1--btn">
-                                <button  className="login--cnt__c1--btn__btn1">
-                                    {none ? 'войти' : 'Создать аккаунт'}
-                                </button>
-                                    <button onClick={() => setNone(!none)}  className="login--cnt__c1--btn__btns">
-
-                                        {none ? 'Регистрация' : 'войти'}
-                                    </button>
-                            </div>
-
+                        </div>
+                        <div className="user-form__btns">
+                            <button className="user-form__btn user-form__btn--active register-form__btn">Создать аккаунт</button>
+                            <button className="user-form__btn register-form__btn">Войти</button>
                         </div>
                     </div>
                 </div>
